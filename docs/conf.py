f698cfc83970ef950572dbeb4ab77fade74699f9
Tony Zoght <tony@zoght.com>
2018-06-05 19:05:02 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
